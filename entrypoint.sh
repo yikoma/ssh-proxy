a32e7e50b60a0030da161c9ba5567a01f602c83d
IKOMA Yoshiki <y@i50.dev>
2024-06-30 22:31:27 +0900

    entrypoint.sh 修正

--- entrypoint.sh
+++ entrypoint.sh
@@ -20,5 +20,5 @@ sed -i "s/<SOCKS_PORT>/${SOCKS_PORT}/g" /privoxy.conf
 
 ssh -gNTC -D ${SOCKS_PORT} -i ${SSH_KEY} \
     -o 'UserKnownHostsFile=/dev/null' -o 'StrictHostKeyChecking=no' \
-	-o 'ServerAliveInterval=300' -o 'ExitOnForwardFailure=yes' $SSH_DEST
+	-o 'ServerAliveInterval=300' -o 'ExitOnForwardFailure=yes' $SSH_DEST &
 privoxy --no-daemon /privoxy.conf
