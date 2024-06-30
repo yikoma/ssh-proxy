1c518dc5b9a3517afbbcd13017dabf67d6d28f85
IKOMA Yoshiki <y@i50.dev>
2024-06-30 22:01:51 +0900

    SSHのオプションを調整

--- entrypoint.sh
+++ entrypoint.sh
@@ -18,5 +18,7 @@ if [[ ! $SOCKS_PORT ]]; then
 fi
 sed -i "s/<SOCKS_PORT>/${SOCKS_PORT}/g" /privoxy.conf
 
-ssh -gNTC -D ${SOCKS_PORT} -o 'ServerAliveInterval=300' -o 'ExitOnForwardFailure=yes' $SSH_DEST
+ssh -gNTC -D ${SOCKS_PORT} -i ${SSH_KEY} \
+    -o 'UserKnownHostsFile=/dev/null' -o 'StrictHostKeyChecking=no' \
+	-o 'ServerAliveInterval=300' -o 'ExitOnForwardFailure=yes' $SSH_DEST
 privoxy --no-daemon /privoxy.conf
